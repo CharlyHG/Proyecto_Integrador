beb3cd1b2b50749f9913389e4484e05335b583fa
charly <charly-970715@hotmail.com>
2023-07-31 21:54:51 -0500

    programa que corra un bucle infinito leyendo e imprimiento las teclas y sólo terminará cuando se presione la tecla ↑ indicada como UP

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Nombre del jugador")
 jugador = input()
-print(f"Estamos muy felices de tenerte {jugador}, ¡felicitaciones y bienvenido! ¡Bienvenido a tu nuevo equipo! Estamos muy felices de que formes parte del juego. ¡Qué alegría tenerte con nosotros, bienvenido {jugador}!")
+print(f"Estamos muy felices de tenerte {jugador},git ¡Bienvenido a tu nuevo equipo! Estamos muy felices de que formes parte del juego. ¡Qué alegría tenerte con nosotros, bienvenido {jugador}!")
+
+import readchar
+
+while True:
+    keypress = readchar.readkey()
+    print(keypress)
+    if keypress == readchar.key.UP:
+        break
+   
